50d823719121d78de42c0aba6c95a2d1a16097ca
teyter <teitur93@gmail.com>
2024-02-06 21:08:40 +0000

    fuse

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Image,
+  Input,
+} from "@chakra-ui/react";
+import Fuse from "fuse.js";
+
+let DATA = [];
 
 function App() {
+  const [cardsData, setCardsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.scryfall.com/cards/search?order=set&q=set:ltr`)
+      .then((response) => {
+        // console.log(response.data);
+        setCardsData(response.data.data);
+        DATA = response.data.data;
+        response.data.data.map((card) => {
+          /**
+          DATA.push({
+            name: card.name,
+            image: card.image_uris.small,
+            colors: card.colors,
+            type: card.type_line,
+          });
+           */
+        });
+
+        console.log("data", DATA);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const CardList = () => {
+    return (
+      <Flex flexWrap="wrap" justify="space-around">
+        {cardsData.map((card) => (
+          <Card maxW="sm">
+            <CardBody>
+              <Image src={card.image_uris.small} borderRadius="sm" />
+            </CardBody>
+          </Card>
+        ))}
+      </Flex>
+    );
+  };
+
+  const handleSearch = (input) => {
+      const options = {
+        threshold: 0.6,
+        keys: ["name"],
+      };
+      const fuse = new Fuse(DATA, options);
+      const results = fuse.search(input);
+
+      if (Object.keys(results).length === 0) {
+        if (input) {
+          setCardsData([]); // ef leit skilar engu, syna ekkert
+        } else {
+          setCardsData(DATA); // ef leit er tomt, syna allt
+        }
+      }
+      else {
+        const fuseToTableData = [];
+        results.map((x) => fuseToTableData.push(x.item));
+        setCardsData(fuseToTableData);
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box margin="0px auto" w="80vw" h="85vh">
+        <Box margin="20px">
+          <Input placeholder="Search" onChange={(e) => handleSearch(e.target.value)} />
+        </Box>
+        <CardList />
+      </Box>
+    </ChakraProvider>
   );
 }
 
